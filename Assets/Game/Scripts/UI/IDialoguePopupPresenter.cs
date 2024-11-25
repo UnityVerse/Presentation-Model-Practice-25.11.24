d39920d87620c4f110f9f6c6d16888760c3f43da
Yaroslav Sarchuk <sarchuk.yav@gmail.com>
2024-11-25 22:08:24 +0400

    Contract

---
+++ Assets/Game/Scripts/UI/IDialoguePopupPresenter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Scripts.UI
+{
+    public interface IDialoguePopupPresenter
+    {
+        event Action OnStateChanged;
+        event Action OnDialogFinished;
+        
+        Sprite CharacterIcon { get; }
+        string CurrentText { get; }
+        DialoguePopupPresenter.ChoicePresenter[] Choices { get; }
+        
+        public interface IChoiceItemPresenter
+        {
+            string Text { get; }
+            void OnClick();
+        }
+    }
+}
